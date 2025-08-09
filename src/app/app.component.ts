b04be0088ba6529541e38231f5c4e5db7362c637
JohnTwiiX <john.fieweger98@gmail.com>
2025-08-09 18:49:05 +0200

    feat: add safe area with plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { SafeArea } from 'capacitor-plugin-safe-area';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,19 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.platform.ready().then(() => {
+      SafeArea.getSafeAreaInsets().then((data) => {
+        const { insets } = data;
+        document.body.style.setProperty('--ion-safe-area-top', `${insets.top}px`);
+        document.body.style.setProperty('--ion-safe-area-right', `${insets.right}px`);
+        document.body.style.setProperty('--ion-safe-area-bottom', `${insets.bottom}px`);
+        document.body.style.setProperty('--ion-safe-area-left', `${insets.left}px`);
+
+        // alert(JSON.stringify(insets));
+      });
+    });
+  }
+
+
 }
