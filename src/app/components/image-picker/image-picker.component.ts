5ce700da5b9c79e1c84b34f3350b679b31c25f6a
TwiiX <john.fieweger98@gmail.com>
2025-11-30 12:52:43 +0100

    feat: add cropping for image picker

--- src/app/components/image-picker/image-picker.component.ts
+++ src/app/components/image-picker/image-picker.component.ts
@@ -1,20 +1,41 @@
 import { db } from '@/services/app-database.service';
 import { loadNativeImage } from '@/utils/image.utils';
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Capacitor } from '@capacitor/core';
-import { IonIcon, IonImg, IonText, IonActionSheet } from '@ionic/angular/standalone';
+import { IonActionSheet, IonButton, IonButtons, IonContent, IonFooter, IonHeader, IonIcon, IonImg, IonModal, IonText, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ImageCroppedEvent, ImageCropperComponent } from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-image-picker',
   templateUrl: 'image-picker.component.html',
   styleUrl: 'image-picker.component.scss',
-  imports: [IonIcon, IonImg, IonText, IonActionSheet],
+  imports: [
+    IonIcon,
+    IonImg,
+    IonText,
+    IonActionSheet,
+    IonModal,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonButton,
+    IonContent,
+    IonFooter,
+    ImageCropperComponent,
+  ],
 })
 export class ImagePickerComponent implements OnChanges {
   @Input() value!: string | undefined;
   @Output() valueChange = new EventEmitter<string>();
   image: string | undefined = undefined;
+
+  @ViewChild('cropModal') cropModal!: IonModal;
+
+  isCropModalOpen = false;
+  croppingImage: File | null = null;
+  croppedImage: string | null = null;
 
   async ngOnChanges(changes: SimpleChanges) {
     if (changes['value'] && this.value) {
@@ -46,8 +67,6 @@ export class ImagePickerComponent implements OnChanges {
     }
     return 'temporary';
   }
-
-  constructor() {}
 
   public actionSheetButtons = [
     {
@@ -90,12 +109,37 @@ export class ImagePickerComponent implements OnChanges {
         resultType: CameraResultType.Uri,
         source,
       });
-
-      this.value = photo.webPath!;
-      this.valueChange.emit(this.value);
+      if (photo.webPath) {
+        const response = await fetch(photo.webPath);
+        const blob = await response.blob();
+        this.croppingImage = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+        this.cropModal.present();
+      }
     } catch (error) {
       console.error('Image pick failed:', error);
     }
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    if (event.objectUrl) {
+      this.croppedImage = event.objectUrl;
+    }
+  }
+
+  confirmCrop() {
+    if (this.croppedImage) {
+      this.value = this.croppedImage;
+      this.valueChange.emit(this.value);
+      this.cropModal.dismiss();
+      this.croppingImage = null;
+      this.croppedImage = null;
+    }
+  }
+
+  cancelCrop() {
+    this.cropModal.dismiss();
+    this.croppingImage = null;
+    this.croppedImage = null;
   }
 
   deleteImage(event: Event) {
