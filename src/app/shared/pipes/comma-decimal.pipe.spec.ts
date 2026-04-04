c09a63bd28462e916d8f8943c75c0797e77c9717
TwiiX <john.fieweger98@gmail.com>
2026-04-04 19:48:42 +0200

    chore: remove unused spec files and update angular.json for schematics configuration

--- src/app/shared/pipes/comma-decimal.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CommaDecimalPipe } from '@/shared/pipes/comma-decimal.pipe';
-
-describe('commaDecimalPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CommaDecimalPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
